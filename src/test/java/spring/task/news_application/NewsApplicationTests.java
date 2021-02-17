81a9b36f0cbb217f5bf74ca3241387d280915e28
adima <adima4347@gmail.com>
2021-02-17 15:07:50 +0300

    Test

---
+++ src/test/java/spring/task/news_application/NewsApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.task.news_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
